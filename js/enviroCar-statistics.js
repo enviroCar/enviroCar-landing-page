4685ea26f619038a6d03c2b15e6e12f8938a8f56
MojioMS <MaurinRadtke@googlemail.com>
2017-10-05 18:23:28 +0200

    lots of updates

---
+++ js/enviroCar-statistics.js
@@ -0,0 +1,44 @@
+numberUsers = 0;
+numberTracks = 0;
+numberMeasurements = 0;
+
+function getStatistics() {
+
+    // get number users:
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", "https://enviroCar.org/api/stable/users?limit=1", true);
+    xmlHttp.setRequestHeader('contentType', 'application/json');
+    xmlHttp.setRequestHeader('X-User', 'naveen-gsoc');
+    xmlHttp.setRequestHeader('X-Token', 'naveen-gsoc123');
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            numberUsers = this.getResponseHeader('Content-Range').split('/')[1];
+        }
+    };
+    xmlHttp.send(null);
+
+    // get number tracks:
+    var xmlHttp2 = new XMLHttpRequest();
+    xmlHttp2.open("GET", "https://enviroCar.org/api/stable/tracks?limit=1", true);
+    xmlHttp2.setRequestHeader('contentType', 'application/json');
+    xmlHttp2.onreadystatechange = function () {
+        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
+            numberTracks = this.getResponseHeader('Content-Range').split('/')[1];
+        }
+    };
+    xmlHttp2.send(null);
+
+    // get number measurements:
+    var xmlHttp3 = new XMLHttpRequest();
+    xmlHttp3.open("GET", "https://enviroCar.org/api/stable/measurements?limit=1", true);
+    xmlHttp3.setRequestHeader('contentType', 'application/json');
+    xmlHttp3.onreadystatechange = function () {
+        if (xmlHttp3.readyState === 4 && xmlHttp3.status === 200) {
+            numberMeasurements = this.getResponseHeader('Content-Range').split('/')[1];
+        }
+    };
+    xmlHttp3.send(null);
+
+}
+
+getStatistics();
---
+++ js/scroll-count.js
@@ -0,0 +1,100 @@
+var a = 0;
+$(window).scroll(function () {
+
+    var oTop = $('#counter').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+        $('.counter-users').each(function () {
+            var $this = $(this),
+                    countTo = numberUsers;
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                    {
+
+                        duration: 5000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            var str = this.countNum+"";
+                            var hundreds = str.substring(str.length-3,str.length);
+                            var thousands = str.substring(str.length-6,str.length-3);
+                            var millions = str.substring(str.length-9,str.length-6);
+                            if (millions.length > 0)
+                                millions = millions + ".";
+                            else {
+                                if (thousands > 0)
+                                    thousands = thousands + ".";
+                            }
+                            var text = millions + thousands + hundreds;
+                            $this.text(text);
+                        }
+
+                    });
+        });
+        $('.counter-tracks').each(function () {
+            var $this = $(this),
+                    countTo = numberTracks;
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                    {
+
+                        duration: 5000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            var str = this.countNum+"";
+                            var hundreds = str.substring(str.length-3,str.length);
+                            var thousands = str.substring(str.length-6,str.length-3);
+                            var millions = str.substring(str.length-9,str.length-6);
+                            if (millions.length > 0)
+                                millions = millions + ".";
+                            else {
+                                if (thousands > 0)
+                                    thousands = thousands + ".";
+                            }
+                            var text = millions + thousands + hundreds;
+                            $this.text(text);
+                        }
+
+                    });
+        });
+        $('.counter-measurements').each(function () {
+            var $this = $(this),
+                    countTo = numberMeasurements;
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum: countTo
+            },
+                    {
+
+                        duration: 5000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            var str = this.countNum+"";
+                            var hundreds = str.substring(str.length-3,str.length);
+                            var thousands = str.substring(str.length-6,str.length-3);
+                            var millions = str.substring(str.length-9,str.length-6);
+                            var text = millions + "." + thousands + "." + hundreds;
+                            $this.text(text);
+                        }
+
+                    });
+        });
+
+        a = 1;
+    }
+
+});
