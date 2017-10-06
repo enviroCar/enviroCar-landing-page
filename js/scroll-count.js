48564542dde14233cd5e6807f710ab5d3d4b229b
MojioMS <MaurinRadtke@googlemail.com>
2017-10-06 08:05:10 +0200

    scroll anims

--- js/scroll-count.js
+++ js/scroll-count.js
@@ -13,7 +13,7 @@ $(window).scroll(function () {
             },
                     {
 
-                        duration: 5000,
+                        duration: 1000,
                         easing: 'swing',
                         step: function () {
                             $this.text(Math.floor(this.countNum));
@@ -45,7 +45,7 @@ $(window).scroll(function () {
             },
                     {
 
-                        duration: 5000,
+                        duration: 1500,
                         easing: 'swing',
                         step: function () {
                             $this.text(Math.floor(this.countNum));
@@ -77,7 +77,7 @@ $(window).scroll(function () {
             },
                     {
 
-                        duration: 5000,
+                        duration: 2000,
                         easing: 'swing',
                         step: function () {
                             $this.text(Math.floor(this.countNum));
