5a000e34649e3abae5065c79671e873884445d3a
MojioMS <maurinradtke@gmail.com>
2017-11-02 17:32:25 +0100

    stasch requirements 1/2

--- js/traffic_light_change.js
+++ js/traffic_light_change.js
@@ -1,68 +1,27 @@
-var traffic_lights = ['assets/traffic_light_green.png', 'assets/traffic_light_red.png'];
-var timeout_1 = 1200;
-var timeout_2 = 600;
-var timeout_3 = 50;
-var count1 = 1; // red
-var tLight1 = document.getElementById('traffic_light_one');
-var tLight2 = document.getElementById('traffic_light_two');
-var tLight3 = document.getElementById('traffic_light_three');
+allLoaded = function () {
 
-turnLightOne = function ()
-{
-    setInterval(function () {
-        tLight1.src = traffic_lights[count];
-    }, 2000);
-};
-
-turnLightTwo = function () {
-    setInterval(function () {
-        tLight2.src = traffic_lights[count];
-    }, 2000);
-};
-
-turnLightThree = function () {
-    setInterval(function () {
-        count++;
-        if (count >= traffic_lights.length)
-            count = 0;
-        // use this below line if you want images in order.
-        tLight3.src = traffic_lights[count];
-    }, 2000);
-};
-
-
-
-allLoaded = function() {
-    count = 0; // red
-    tLight = traffic_lights[count];
-    console.log(tLight1);
-    console.log(tLight2);
-    console.log(tLight3);
-    tLight1 = document.getElementById('traffic_light_one');
-    tLight2 = document.getElementById('traffic_light_two');
-    tLight3 = document.getElementById('traffic_light_three');
+    var traffic_light1 = document.getElementById("traffic_light_three_g");
+    traffic_light1.className += " traffic_light_anim";
     setTimeout(function () {
         // trigger car:
         var car = document.getElementById("car");
         car.className += " car";
 
+    }, 2200);
+
+    setTimeout(function () {
         
-        tLight3.src = traffic_lights[count];
-        turnLightThree();
+        var traffic_light2 = document.getElementById("traffic_light_two_g");
+        traffic_light2.className += " traffic_light_anim";
+        
+    }, 1000);
 
-        setTimeout(function () {
-            // wait  550ms
-                    
-            tLight2.src = traffic_lights[count];
-            turnLightTwo();
-        }, 550)
-        setTimeout(function () {
-            //wait 1050ms
-            tLight1.src = traffic_lights[count];
-            turnLightOne();
-        }, 1050)
-
-    }, 2000);
+    setTimeout(function () {
+        
+        var traffic_light3 = document.getElementById("traffic_light_one_g");
+        traffic_light3.className += " traffic_light_anim";
+        
+    }, 1700);
 };
 
 
@@ -74,12 +33,12 @@ var loaded = {
     tl3: false
 };
 
-areAllLoaded = function() {
+areAllLoaded = function () {
     console.log(loaded);
     return (loaded.street &&
             loaded.car &&
             loaded.tl1 &&
-            loaded.tl2 && 
+            loaded.tl2 &&
             loaded.tl3);
 };
 
@@ -89,47 +48,47 @@ var tl1 = document.querySelector('#traffic_light_one');
 var tl2 = document.querySelector('#traffic_light_two');
 var tl3 = document.querySelector('#traffic_light_three');
 
-streetLoaded = function() {
+streetLoaded = function () {
     loaded.street = true;
     console.log("street img laoded");
     if (areAllLoaded())
         allLoaded();
     street.removeEventListener('load', streetLoaded);
 };
-carLoaded = function() {
+carLoaded = function () {
     loaded.car = true;
     console.log("car img laoded");
     if (areAllLoaded())
         allLoaded();
     car.removeEventListener('load', carLoaded);
 };
-tl1Loaded = function() {
+tl1Loaded = function () {
     loaded.tl1 = true;
     console.log("tl1 img laoded");
     if (areAllLoaded())
         allLoaded();
     tl1.removeEventListener('load', tl1Loaded);
 };
-tl2Loaded = function() {
+tl2Loaded = function () {
     loaded.tl2 = true;
     console.log("tl2 img laoded");
     if (areAllLoaded())
         allLoaded();
     tl2.removeEventListener('load', tl2Loaded);
 };
-tl3Loaded = function() {
+tl3Loaded = function () {
     loaded.tl3 = true;
     console.log("tl3 img laoded");
     if (areAllLoaded())
         allLoaded();
-    tl3.removeEventListener('load',tl3Loaded);
+    tl3.removeEventListener('load', tl3Loaded);
 };
 
 if (street.complete) {
     streetLoaded();
 } else {
     street.addEventListener('load', streetLoaded);
-    street.addEventListener('error', function() {
+    street.addEventListener('error', function () {
         console.log('error loading street img!');
     });
 }
@@ -137,7 +96,7 @@ if (car.complete) {
     carLoaded();
 } else {
     car.addEventListener('load', carLoaded);
-    car.addEventListener('error', function() {
+    car.addEventListener('error', function () {
         console.log('error loading car img!');
     });
 }
@@ -145,7 +104,7 @@ if (tl1.complete) {
     tl1Loaded();
 } else {
     tl1.addEventListener('load', tl1Loaded);
-    tl1.addEventListener('error', function() {
+    tl1.addEventListener('error', function () {
         console.log('error loading tl1 img!');
     });
 }
@@ -153,7 +112,7 @@ if (tl2.complete) {
     tl2Loaded();
 } else {
     tl2.addEventListener('load', tl2Loaded);
-    tl2.addEventListener('error', function() {
+    tl2.addEventListener('error', function () {
         console.log('error loading tl2 img!');
     });
 }
@@ -161,7 +120,7 @@ if (tl3.complete) {
     tl3Loaded();
 } else {
     tl3.addEventListener('load', tl3Loaded);
-    tl3.addEventListener('error', function() {
+    tl3.addEventListener('error', function () {
         console.log('error loading tl3 img!');
     });
 }
