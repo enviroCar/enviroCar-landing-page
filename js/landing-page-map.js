fb18f977a96abb428ddb7379834db7dd4c340e41
MojioMS <maurinradtke@gmail.com>
2017-12-15 15:37:52 +0100

    page updates

---
+++ js/landing-page-map.js
@@ -0,0 +1,27 @@
+var layers = [
+    new ol.layer.Tile({
+        source: new ol.source.OSM()
+    }),
+    new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+            url: 'http://processing.envirocar.org:9090/geoserver/wms',
+            port: 9090,
+            params: {'LAYERS': 'cite:tracks'},
+            ratio: 1,
+            serverType: 'geoserver'
+        })
+    })
+];
+var map = new ol.Map({
+    projection: 'EPSG:4326',
+    layers: layers,
+    target: 'map',
+    view: new ol.View({
+        center: [997148, 6569099],
+        zoom: 6
+    })
+});
+
+setTimeout(function () {
+    window.dispatchEvent(new Event('resize'));
+}, 1000);
