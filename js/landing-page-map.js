2dce093b4398eba71c55170b21e7044f7ffe552a
MojioMS <maurinradtke@gmail.com>
2018-01-24 12:53:21 +0100

    translation and map updates

--- js/landing-page-map.js
+++ js/landing-page-map.js
@@ -1,33 +1,56 @@
-var layer = new ol.layer.Tile({
-    source: new ol.source.OSM()
-});
-
-var layers = [
-    new ol.layer.Tile({
-        source: new ol.source.OSM()
-    }),
-    new ol.layer.Image({
-        source: new ol.source.ImageWMS({
-            url: 'http://processing.envirocar.org:9090/geoserver/wms',
-            port: 9090,
-            params: {
-                'LAYERS': 'cite:roadsegments',
-                'STYLES': 'speedinterpolation'
-            },
-            ratio: 1,
-            serverType: 'geoserver'
+function initialize() {
+    var layers = [
+//        new ol.layer.Tile({
+//            source: new ol.source.Stamen({
+//                layer: 'toner'
+//            })
+//        }),
+        new ol.layer.Tile({
+           source: new ol.source.OSM({
+               url: "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+           }) 
+        }),
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'co2interpolation'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        }),
+        new ol.layer.Image({
+            source: new ol.source.ImageWMS({
+                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                port: 9090,
+                params: {
+                    'LAYERS': 'cite:roadsegments',
+                    'STYLES': 'speedinterpolation'
+                },
+                ratio: 1,
+                serverType: 'geoserver'
+            })
         })
-    })
-];
-var map = new ol.Map({
-    projection: 'EPSG:4326',
-    layers: layers,
-    target: 'map',
-    view: new ol.View({
-        center: [997148, 6569099],
-        zoom: 6
-    })
-});
+    ];
+    var map = new ol.Map({
+        layers: layers,
+        target: 'map',
+        view: new ol.View({
+            center: [1000000, 6700000],
+            zoom: 6
+        }),
+        controls: ol.control.defaults({
+          zoom: false,
+          attribution: false,
+          rotate: false
+        })
+    });
+}
+
+initialize();
 
 
 setTimeout(function () {
