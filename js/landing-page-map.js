7f2ddca8c43c0992d25599dc7e9a01fc3ee4ba70
MojioMS <maurinradtke@gmail.com>
2018-01-31 16:34:16 +0100

    fix issue #5

--- js/landing-page-map.js
+++ js/landing-page-map.js
@@ -27,6 +27,7 @@ function initialize() {
                 url: 'http://processing.envirocar.org:9090/geoserver/wms',
                 port: 9090,
                 params: {
+//                    'LAYERS': 'cite:tracks'
                     'LAYERS': 'cite:roadsegments',
                     'STYLES': 'speedinterpolation'
                 },
@@ -58,7 +59,9 @@ function initialize() {
           zoom: true,
           attribution: true,
           rotate: true
-        }),
+        }).extend([
+            new ol.control.FullScreen()
+        ]),
         interactions: ol.interaction.defaults({
             mouseWheelZoom: false
         })
@@ -66,6 +69,143 @@ function initialize() {
 }
 
 initialize();
+function initialize2() {
+    var layers = [
+//        new ol.layer.Tile({
+//            source: new ol.source.Stamen({
+//                layer: 'toner'
+//            })
+//        }),
+        new ol.layer.Tile({
+           source: new ol.source.OSM({
+               url: "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+           }) 
+        }),
+        new ol.layer.Image({
+            source: new ol.source.ImageWMS({
+                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                port: 9090,
+                params: {
+                    'LAYERS': 'cite:roadsegments',
+                    'STYLES': 'co2interpolation'
+                },
+                ratio: 1,
+                serverType: 'geoserver'
+            })
+        }),
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'speedinterpolation'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        }),
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'speedinterpolions'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        })
+    ];
+    var map2 = new ol.Map({
+        layers: layers,
+        target: 'map2',
+        view: new ol.View({
+            center: [1000000, 6700000],
+            zoom: 6
+        }),
+        controls: ol.control.defaults({
+          zoom: true,
+          attribution: true,
+          rotate: true
+        }),
+        interactions: ol.interaction.defaults({
+            mouseWheelZoom: false
+        })
+    });
+}
+
+initialize2();
+
+function initialize3() {
+    var layers = [
+        new ol.layer.Tile({
+            source: new ol.source.Stamen({
+                layer: 'toner'
+            })
+        }),
+//        new ol.layer.Tile({
+//           source: new ol.source.OSM({
+//               url: "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+//           }) 
+//        }),
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'co2interpolation'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        }),
+        new ol.layer.Image({
+            source: new ol.source.ImageWMS({
+                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                port: 9090,
+                params: {
+                    'LAYERS': 'cite:roadsegments',
+                    'STYLES': 'speedinterpolation'
+                },
+                ratio: 1,
+                serverType: 'geoserver'
+            })
+        }),
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'speedinterpolions'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        })
+    ];
+    var map3 = new ol.Map({
+        layers: layers,
+        target: 'map3',
+        view: new ol.View({
+            center: [1000000, 6700000],
+            zoom: 6
+        }),
+        controls: ol.control.defaults({
+          zoom: true,
+          attribution: true,
+          rotate: true
+        }),
+        interactions: ol.interaction.defaults({
+            mouseWheelZoom: false
+        })
+    });
+}
+
+initialize3();
 
 
 setTimeout(function () {
---
+++ js/resource-maps.js
@@ -0,0 +1,123 @@
+function initialize() {
+    var defLayer = new ol.layer.Tile({
+        source: new ol.source.OSM({
+            url: "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
+        })
+    });
+
+    var defView = new ol.View({
+        center: [1000000, 6700000],
+        zoom: 6
+    });
+
+//    var layers2 = [
+//        new ol.layer.Image({
+//            source: new ol.source.ImageWMS({
+//                url: 'http://processing.envirocar.org:9090/geoserver/wms',
+//                port: 9090,
+//                params: {
+//                    'LAYERS': 'cite:roadsegments',
+//                    'STYLES': 'co2interpolation'
+//                },
+//                ratio: 1,
+//                serverType: 'geoserver'
+//            })
+//        }),
+//    ];
+
+    var mapres1 = new ol.Map({
+        layers: [
+            defLayer,
+            new ol.layer.Image({
+                source: new ol.source.ImageWMS({
+                    url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                    port: 9090,
+                    params: {
+                        'LAYERS': 'cite:roadsegments',
+                        'STYLES': 'speedinterpolation'
+                    },
+                    ratio: 1,
+                    serverType: 'geoserver'
+                })
+            })
+        ],
+        target: 'mapres1',
+        view: defView,
+        controls: ol.control.defaults({
+            zoom: true,
+            attribution: true,
+            rotate: true
+        }).extend([
+            new ol.control.FullScreen()
+        ])
+        ,interactions: ol.interaction.defaults({
+            mouseWheelZoom: false
+        })
+    }
+    );
+    var mapres2 = new ol.Map({
+        target: 'mapres2',
+        layers: [defLayer,
+            new ol.layer.Image({
+                source: new ol.source.ImageWMS({
+                    url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                    port: 9090,
+                    params: {
+                        'LAYERS': 'cite:roadsegments',
+                        'STYLES': 'co2interpolation'
+                    },
+                    ratio: 1,
+                    serverType: 'geoserver'
+                })
+            })
+        ],
+        view: defView,
+        controls: ol.control.defaults({
+            zoom: true,
+            attribution: true,
+            rotate: true
+        }).extend([
+            new ol.control.FullScreen()
+        ])
+        ,interactions: ol.interaction.defaults({
+            mouseWheelZoom: false
+        })
+    });
+    var mapres3 = new ol.Map({
+        target: 'mapres3',
+        layers: [defLayer,
+            new ol.layer.Image({
+                source: new ol.source.ImageWMS({
+                    url: 'http://processing.envirocar.org:9090/geoserver/wms',
+                    port: 9090,
+                    params: {
+                        'LAYERS': 'cite:roadsegments',
+                        'STYLES': 'consumptioninterpolation'
+                    },
+                    ratio: 1,
+                    serverType: 'geoserver'
+                })
+            })
+        ],
+        view: defView,
+        controls: ol.control.defaults({
+            zoom: true,
+            attribution: true,
+            rotate: true
+        }).extend([
+            new ol.control.FullScreen()
+        ])
+        ,interactions: ol.interaction.defaults({
+            mouseWheelZoom: false
+        })
+    });
+}
+
+initialize();
+
+setTimeout(function () {
+    window.dispatchEvent(new Event('resize'));
+    setTimeout(function () {
+        window.dispatchEvent(new Event('resize'));
+    }, 1000);
+}, 1000);
