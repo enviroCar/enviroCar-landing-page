a736ef0c33843f70425703da6c939835132ec9aa
Christian Autermann <c.autermann@52north.org>
2019-12-04 20:50:18 +0100

    use new api for entity counts

--- js/enviroCar-statistics.js
+++ js/enviroCar-statistics.js
@@ -3,42 +3,24 @@ numberTracks = 0;
 numberMeasurements = 0;
 
 function getStatistics() {
+  // get number users:
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", "https://enviroCar.org/api/dev/", true);
+  xmlHttp.setRequestHeader("Accept", "application/json");
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      var json;
 
-    // get number users:
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", "https://enviroCar.org/api/stable/users?limit=1", true);
-    xmlHttp.setRequestHeader('Accept', 'application/json');
-    xmlHttp.setRequestHeader('X-User', 'deleteMe');
-    xmlHttp.setRequestHeader('X-Token', '1');
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-            numberUsers = this.getResponseHeader('Content-Range').split('/')[1];
-        }
-    };
-    xmlHttp.send();
-
-    // get number tracks:
-    var xmlHttp2 = new XMLHttpRequest();
-    xmlHttp2.open("GET", "https://enviroCar.org/api/stable/tracks?limit=1", true);
-    xmlHttp2.setRequestHeader('Accept', 'application/json');
-    xmlHttp2.onreadystatechange = function () {
-        if (xmlHttp2.readyState === 4 && xmlHttp2.status === 200) {
-            numberTracks = this.getResponseHeader('Content-Range').split('/')[1];
-        }
-    };
-    xmlHttp2.send();
-
-    // get number measurements:
-    var xmlHttp3 = new XMLHttpRequest();
-    xmlHttp3.open("GET", "https://enviroCar.org/api/stable/measurements?limit=1", true);
-    xmlHttp3.setRequestHeader('Accept', 'application/json');
-    xmlHttp3.onreadystatechange = function () {
-        if (xmlHttp3.readyState === 4 && xmlHttp3.status === 200) {
-            numberMeasurements = this.getResponseHeader('Content-Range').split('/')[1];
-        }
-    };
-    xmlHttp3.send();
-
+      if (xmlHttp.responseType === "json") {
+        json = xmlHttp.response;
+      } else {
+        json = JSON.parse(xmlHttp.responseText);
+      }
+      numberUsers = json.counts.users;
+      numberTracks = json.counts.tracks;
+      numberMeasurements = json.counts.measurements;
+    }
+  };
+  xmlHttp.send();
 }
-
 getStatistics();
